f6227088ce53fd483f2989f04fea954d5087718a
Sasha <sasha.lorion@gmail.com>
2024-03-23 18:28:28 +0100

    [WIP] - Contact list

--- app/src/main/java/com/example/ft_hangouts/ContactList.java
+++ app/src/main/java/com/example/ft_hangouts/ContactList.java
@@ -2,8 +2,12 @@ package com.example.ft_hangouts;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.Gravity;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ft_hangouts.fragment.HeaderFragment;
 
@@ -21,15 +26,10 @@ public class ContactList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_list);
 
-        // Récupérer une référence à votre LinearLayout racine
-
         FrameLayout frameLayout = findViewById(R.id.fragment_header);
-        // Créer une instance de HeaderFragment
         HeaderFragment headerFragment = new HeaderFragment();
-
-        // Ajouter le fragment au conteneur
-        getSupportFragmentManager().beginTransaction()
-                .add(frameLayout.getId(), headerFragment)
-                .commit();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(frameLayout.getId(), headerFragment);
+        transaction.commit();
     }
 }
