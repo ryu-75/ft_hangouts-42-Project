e3bcaf712888d8d8d43be925b68a62837595ea27
Sasha <sasha.lorion@gmail.com>
2024-03-26 14:23:29 +0100

    [ADD] - manage back button

--- app/src/main/java/com/example/ft_hangouts/ui/AddContactActivity.java
+++ app/src/main/java/com/example/ft_hangouts/ui/AddContactActivity.java
@@ -1,26 +1,58 @@
 package com.example.ft_hangouts.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.ft_hangouts.R;
+import com.example.ft_hangouts.fragment.HeaderFragment;
 
 public class AddContactActivity extends AppCompatActivity {
-
+    ConstraintLayout popUpContainer;
+    ImageView   backArrowBtn;
+    Button      discardBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_contact);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        popUpContainer = findViewById(R.id.popup_contact);
+        popUpContainer.setVisibility(View.VISIBLE);
+
+        backArrowBtn = findViewById(R.id.back_btn);
+        backArrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
+
+        discardBtn = findViewById(R.id.popup_btn_error);
+
+        discardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
